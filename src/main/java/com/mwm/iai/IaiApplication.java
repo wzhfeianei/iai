887bd37eb60a94a95db25fee4b71b80e29ebcaa1
weizhanfei <zhanfei.wei@eascs.com>
2018-01-15 14:49:10 +0800

    init

---
+++ src/main/java/com/mwm/iai/IaiApplication.java
@@ -0,0 +1,12 @@
+package com.mwm.iai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaiApplication.class, args);
+	}
+}
